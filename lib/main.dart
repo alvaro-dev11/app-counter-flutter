81966ab47617f6475b9db88eecd9bcb59a569306
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-15 17:19:36 -0500

    Agregar comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:counter_app/screens/counter_screen.dart';
 // import 'package:counter_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
+// function para iniciar mi app
 void main() {
   runApp(const MyApp());
 }
 
+/* 
+clase principal que extiende un widget sin estado,
+quiere decir que no se redibuja en ningun momento
+ */
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
